c34730c59f867e178f733ca470529f57de067d47
AHNAF ALIF TSABITUL 'AZMI DHIYAUL HAQ <ahnafalf05@gmail.com>
2025-11-04 15:54:54 +0700

    commit

--- DisplayTwo1.java
+++ DisplayTwo1.java
@@ -3,17 +3,21 @@ import java.util.Scanner;
 public class DisplayTwo1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int numInput;
-
         System.out.print("Input some number: ");
-        numInput = input.nextInt();
+        int numInput = input.nextInt();
 
         for (int i = 2; i <= numInput; i += 2) {
-            System.out.print(i + " "); // Print on same line with space
+            System.out.print(i + " ");
         }
 
         input.close();
     }
 }
 
-/* */
+/*
+1.modification to Do make the program produce similar result but WITHOUT IF statement. 
+Please insert a screenshot of your code to the report
+Done
+2.Do modification to make the program print like this following result. Please insert a screenshot of your code to the report
+Done
+*/
