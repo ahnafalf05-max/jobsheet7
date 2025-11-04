6fcac01a08e164d997de5d1910c76d6c9cf46c1e
AHNAF ALIF TSABITUL 'AZMI DHIYAUL HAQ <ahnafalf05@gmail.com>
2025-11-04 08:11:54 +0700

    two

--- ForMultiples1.java
+++ ForMultiples1.java
@@ -27,3 +27,9 @@ public class ForMultiples1 {
 }
     
 
+/* 
+1. There are 3 main components in FOR loop. Based on experiment 1 above, identify and explain these 3 components! 
+int i = 1 Sets the starting value of i to 1. This runs once before the loop begins
+i <= 50 Checks if i is less than or equal to 50. If true, the loop continues
+i++ Increments 1 by 1 after each loop cycle. Ensures progress toward ending the loop
+*/ 
