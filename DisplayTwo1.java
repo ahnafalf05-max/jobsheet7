c7be4f3469fdbf02263a4869a51816f93a217f2b
AHNAF ALIF TSABITUL 'AZMI DHIYAUL HAQ <ahnafalf05@gmail.com>
2025-11-04 15:44:02 +0700

    three

--- DisplayTwo1.java
+++ DisplayTwo1.java
@@ -15,3 +15,5 @@ public class DisplayTwo1 {
         input.close();
     }
 }
+
+/* */
--- ForMultiples1.java
+++ ForMultiples1.java
@@ -9,18 +9,22 @@ public class ForMultiples1 {
         int multiple = sc.nextInt();
         int sum = 0;
         int counter = 0;
+        double average = 0.0; // 
 
-        
-        for (int i=1; i <= 50; i++) {
+        for (int i = 1; i <= 50; i++) {
             if (i % multiple == 0) {
-                sum = sum + i;
+                sum += i;
                 counter++;
             }
         }
 
-        
+        if (counter > 0) {
+            average = (double) sum / counter; 
+        }
+
         System.out.printf("There are %d numbers that are multiples of %d in the range 1 to 50.\n", counter, multiple);
         System.out.printf("The sum of all multiples of %d in the range 1 to 50 is %d.\n", multiple, sum);
+        System.out.printf("The average of these multiples is %.2f.\n", average); 
 
         sc.close();
     }
@@ -32,4 +36,5 @@ public class ForMultiples1 {
 int i = 1 Sets the starting value of i to 1. This runs once before the loop begins
 i <= 50 Checks if i is less than or equal to 50. If true, the loop continues
 i++ Increments 1 by 1 after each loop cycle. Ensures progress toward ending the loop
-*/ 
+2. Done
+3. */ 
