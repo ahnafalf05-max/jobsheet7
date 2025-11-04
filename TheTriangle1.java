06aaf37b243198415b29a46a8d486322de86ec7d
AHNAF ALIF TSABITUL 'AZMI DHIYAUL HAQ <ahnafalf05@gmail.com>
2025-11-04 16:02:57 +0700

    commit

--- TheTriangle1.java
+++ TheTriangle1.java
@@ -4,18 +4,30 @@ public class TheTriangle1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int numInput;
-        String s = "";
 
         System.out.print("Input some number: ");
         numInput = input.nextInt();
 
-        for (int i = 0; i < numInput; i++) {
-            s += " *";
-            System.out.println(s);
+        for (int i = 1; i <= numInput; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" *");
+            }
+            System.out.println();
         }
 
         input.close();
     }
 }
     
-
+ /* 
+ 1.Do a modification on the program therefore your program utilize FOR statement rather than WHILE statement. 
+   Done
+ 2.Explain the meaning of s += “ *” and why is it possible? 
+   It means:
+	Take the current value of the string (s)
+ 	Append "*"  to the end of it 
+    Store the new result back into (s)
+    Because in Java:
+ 	The + operator is overloaded to work with strings.
+ 	When you use +  with strings, Java automatically performs concatenation
+  */
